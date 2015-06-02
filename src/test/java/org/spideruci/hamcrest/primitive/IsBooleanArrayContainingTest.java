5ff514876991d6ae06cc644594dd4298e1f11e9d
VijayKrishna Palepu <vijay.0288@gmail.com>
2015-06-01 22:21:59 -0700

    Boolean Primitive Array support

---
+++ src/test/java/org/spideruci/hamcrest/primitive/IsBooleanArrayContainingTest.java
@@ -0,0 +1,15 @@
+package org.spideruci.hamcrest.primitive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class IsBooleanArrayContainingTest {
+
+  @Test @Ignore
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
